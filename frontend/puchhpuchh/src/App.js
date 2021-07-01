54fd4809583b94b8f7ddc0d520aeb72f927b7a3b
akshat799 <gulati.akshat799@gmail.com>
2021-07-01 03:06:59 -0300

    Navbar

---
+++ frontend/puchhpuchh/src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/homePage/Navbar'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header title = "PuchhPuchh" />
+    </div>
+  );
+}
+
+export default App;
