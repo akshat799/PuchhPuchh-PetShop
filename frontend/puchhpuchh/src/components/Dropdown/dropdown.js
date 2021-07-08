33dc5b9c7f40884bc69c5fa8d11501a29300c072
Raghav <raghav.chandak@indusnet.co.in>
2021-07-08 20:56:52 +0530

    Added reusable dropdown

--- frontend/puchhpuchh/src/components/Dropdown/dropdown.js
+++ frontend/puchhpuchh/src/components/Dropdown/dropdown.js
@@ -1,43 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import "./dropdown.css";
 
 function Dropdown(props) {
-  const [selected, setSelected] = useState(false);
-
-  const toggleSelected = () => {
-    let currVal = !selected;
-    setSelected(currVal);
-  };
-
-  console.log("PRINTING PROPS:", props.menu.title);
   return (
-    /*    <div className="custom-dropdown">
-      <div className="main-container">
-        <p className="parent" onClick={toggleSelected}>
-          Dropdown Here
-        </p>
-        <div
-          className="dropdown-elements"
-          style={{ visibility: selected ? "visible" : "hidden" }}
-        >
-          <p>First</p>
-          <p>Second</p>
-        </div>
-      </div>
-    </div> */
     <div className="dropdown-container">
       <div className="option-1">
-        <button>{props.menu.title}</button>
+        <button style={{ color: props.color }}>{props.menu.title}</button>
         <div className="list">
-          {/* <span>Lorem</span>
-          <span>Lorem</span>
-          <span>Lorem</span>
-          <span>Lorem</span>
-          <span>Lorem</span>
-          <span>Lorem</span>
-          <span>Lorem</span> */}
           {props.menu.options.map((option) => {
-            return <p onClick={option.onPress}>{option.name}</p>;
+            return (
+              <p className="menu-options" onClick={option.onPress}>
+                {option.name}
+              </p>
+            );
           })}
         </div>
       </div>
