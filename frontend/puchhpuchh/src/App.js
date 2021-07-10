54a5179bbe3e1643914a04280f1aa0c2a56ce7d5
akshat799 <gulati.akshat799@gmail.com>
2021-07-10 01:43:02 -0300

    add-on

--- frontend/puchhpuchh/src/App.js
+++ frontend/puchhpuchh/src/App.js
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar/Navbar";
 import Dropdown from "./components/Dropdown/dropdown";
-import Footer from "./components/Header/Footer";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
